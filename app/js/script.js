c52af7c3639e4930f9c275aa24c87b4fd8be0b9a
LuckyLam93 <tutrinh3004@gmail.com>
2016-02-15 20:55:46 +0700

    Refactor folder structure

---
+++ app/js/script.js
@@ -0,0 +1,12 @@
+//$(function () {
+//  $('#datetimepickerin').datetimepicker();
+//  $('#datetimepickerout').datetimepicker({
+//      useCurrent: false //Important! See issue #1075
+//  });
+//  $("#datetimepickerin").on("dp.change", function (e) {
+//      $('#datetimepickerout').data("DateTimePicker").minDate(e.date);
+//  });
+//  $("#datetimepickerout").on("dp.change", function (e) {
+//      $('#datetimepickerin').data("DateTimePicker").maxDate(e.date);
+//  });
+//});
