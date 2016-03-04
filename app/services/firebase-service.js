e4d3fafa444ccbd40e9be42112405f3bdb9c1ca0
LuckyLam93 <tutrinh3004@gmail.com>
2016-03-04 23:17:32 +0700

    cleaned

--- app/services/firebase-service.js
+++ app/services/firebase-service.js
@@ -46,7 +46,6 @@ var FirebaseService = (function() {
             name: tempData[i].hotel_name,
             id: tempData[i].$id
           }); 
-//          console.log(data);
         }
       }
     }).catch(function(error) {
@@ -54,7 +53,6 @@ var FirebaseService = (function() {
     });
     
     return data;
-    
   }
   
   FirebaseService.prototype.getDetailData = function(id) {
@@ -68,66 +66,8 @@ var FirebaseService = (function() {
     console.log('hey');
     var ref = new Firebase(this.originalLink);
     
-    console.log(this.$firebaseAuth(ref));
     return this.$firebaseAuth(ref);
-//    return ref.authWithOAuthPopup('google', function(error, authData) {
-//      if (error) {
-//        console.log('Login Failed', error);
-//      } else {
-//        console.log('Authenticated successfully with payload:', authData);
-//        
-//      }
-//    }, {
-//      remember: 'sessionOnly',
-//      scope: 'email'
-//    });
-    
-    
   };
-  
-  //demo
-  FirebaseService.prototype.setDemo = function() {
-    var myId = '';
-    var ref = new Firebase('https://sunshine-333.firebaseio.com/users');
-
-    var data = this.$firebaseArray(ref);
-    data.$add({
-        FUCKKKK: {
-          friends: { brinchen: true },
-          name: {
-						name1: {
-									though1:"great",
-									though2:"good"
-								}
-					},
-          
-          widgets: { one: true, three: true }
-        }}).then(function(ref) {
-          var id = ref.key();
-          console.log("added record with id " + id);
-          myId = id;
-          console.log(data.$indexFor(id)); // returns location in the array
-        });
-    
-        console.log('my id:', myId);
-        
-        return myId;
-//    var newRef = ref.push();
-//    newRef.set({
-//        a: {
-//          friends: { brinchen: true },
-//          name: {
-//						name1: {
-//									though1:"great",
-//									though2:"good"
-//								}
-//					},
-//          
-//          widgets: { one: true, three: true }
-//        }});
-//    var postID = newRef.key();
-//    return postID;  
-  }
   
   return FirebaseService;
 })();
